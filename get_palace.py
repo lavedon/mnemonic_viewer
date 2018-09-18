1b3600b17c4a78f1c4a6687de5a3093714eddcbd
lavedon <lavedon@gmail.com>
2018-09-18 08:31:59 -0400

    select function works.  Now to make a loading screen

--- get_palace.py
+++ get_palace.py
@@ -4,7 +4,9 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 class Memory_Palace:
     def __init__(self, sheet_select):
-        palace = get_palace(sheet_select)
+        self.sheets = [] 
+
+        palace = self.get_palace(sheet_select)
         self.loci = palace.col_values(1)
         self.facts = palace.col_values(2)
         self.mnemonics = palace.col_values(3)
@@ -16,18 +18,21 @@ class Memory_Palace:
         del self.mnemonics[0]
         del self.image_files[0]
 
-def get_palace(sheet_select):
-    scope = ['https://spreadsheets.google.com/feeds',
-             'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-    client = gspread.authorize(creds)
-    # Open passed through sheet name
+        #set the list of sheets
 
-    wk_book = client.open_by_key('1ynNFt25lPaMsjj3RPk0LH6hdia1qRWxX1rdNPIzp1f0')
-    sheets = wk_book.worksheets() 
-    palace = sheets[sheet_select]
-    return palace
+
+    def get_palace(self, sheet_select):
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+        creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+        client = gspread.authorize(creds)
+        # Open passed through sheet name
+
+        wk_book = client.open_by_key('1ynNFt25lPaMsjj3RPk0LH6hdia1qRWxX1rdNPIzp1f0')
+        self.sheets = wk_book.worksheets() 
+        palace = self.sheets[sheet_select]
+        return palace
 
 
 if __name__ == "__main__":
-    testPalace = Memory_Palace(0)
+    testPalace = Memory_Palace(1)
--- viewer_1.py
+++ viewer_1.py
@@ -1,5 +1,5 @@
-import logging
 import pdb
+import logging
 
 logging.basicConfig(filename='viewer.log', filemode='w', format='%(levelname)s:%(asctime)s %(message)s', level=logging.DEBUG)
 
@@ -23,9 +23,7 @@ class App:
                 self.text_surface3_flag = False
                 self.image_flag = True 
 
-                self.select_which = 0
-                self.select_menu = None
-                self.palaces = ["Sales", "History", "Coding"]
+                self.palaces = ["History", "Sales", "Coding"]
 
         def on_init(self):
                 pygame.init()
@@ -64,25 +62,42 @@ class App:
                             self.image_count += 1
                         elif self.joystick.get_button(6):
                             logging.info('Controller button 6 pressed.')
+                            theData.select_which += 1
+                            if theData.select_menu_open == False:
+                                theData.select_menu_open = True
+                            if theData.select_which > len(theData.sheet_list):
+                                theData.select_which = 0
+                                theData.select_menu_open = False
                         elif self.joystick.get_button(7):
                             logging.info('Controller button 7 pressed.')
+                            if theData.select_menu_open:
+                                theData.select_menu_open = False
+                                theData.get_from_palace() 
+                                theData.start_up = False
+                                theData.get_images()
 
         def on_render(self):
                 black = 0, 0, 0
 
                 self.display_surf.fill(black)
-                if self.image_flag == True:
-                    self.display_surf.blit(theData.image_list_surf[self.image_count], (0, 0))
-                if self.text_surface1_flag == True:
-                    self.display_surf.blit(theData.loci_list_surf[self.image_count], (50, 40))
-                if self.text_surface2_flag == True:
-                    self.display_surf.blit(theData.facts_list_surf[self.image_count], (50, 120))
-                if self.text_surface3_flag == True:
-                    self.display_surf.blit(theData.mnemonics_list_surf[self.image_count], (50, 250))
-                if self.select_which > 0:
-                    self.display_surf.blit(self.select_menu, (250, 250))
+
+                if theData.start_up:
+                    self.display_surf.blit(theData.select_menu(), (250, 250))
+                else:
+                    if self.image_flag:
+                        self.display_surf.blit(theData.image_list_surf[self.image_count], (0, 0))
+                    if self.text_surface1_flag:
+                        self.display_surf.blit(theData.loci_list_surf[self.image_count], (50, 40))
+                    if self.text_surface2_flag:
+                        self.display_surf.blit(theData.facts_list_surf[self.image_count], (50, 120))
+                    if self.text_surface3_flag:
+                        self.display_surf.blit(theData.mnemonics_list_surf[self.image_count], (50, 250))
+                    if theData.select_menu_open:
+                        self.display_surf.blit(theData.select_menu(), (250, 250))
 
                 pygame.display.flip()
+
+
 
         def on_cleanup(self):
                 pygame.quit()
@@ -113,39 +128,76 @@ class Data:
                 self.facts_list_surf = []
                 self.mnemonics_list = [] 
                 self.mnemonics_list_surf = [] 
-                self.image_files_list = [] 
+                self.image_files_list = []
                 self.image_rect = None
 
+
+                self.start_up = True
+                self.select_which = 0
+                self.select_menu_open = False
+
+                self.sheet_list = []
+                #  the local location of folders with various palace images
+                self.folder_locations = ['C:\\Users\\Luke\\Desktop\\Memory Palace\\Palaces\\Dredd',
+                        'C:\\Users\\Luke\\Desktop\\Memory Palace\\Palaces', 
+                        'C:\\Users\\Luke\\Desktop\Memory Palace\\Palaces\\Computer\\Star Trek 25th Anniversary']
+               
 
                 pygame.init()
                 self.font_1 = pygame.font.Font(None, 72)
                 self.font_2 = pygame.font.Font(None, 52)
                 self.get_from_palace()
-                self.get_images()
+                if self.start_up:
+                    self.select_menu()
+                else:
+                    self.get_images()
+
+
+        def select_menu(self):
+
+            self.select_menu_open = True 
+            surface = pygame.Surface((500, 500))
+            surface.fill((255, 255, 255))
+
+            # Render all the possible sheets in a box
+            line_position = 10
+            for count, line in enumerate(self.sheet_list): 
+                if count == self.select_which:
+                    rendered_palace = self.font_1.render(line.title, False, (255, 0, 0))
+                else:
+                    rendered_palace = self.font_1.render(line.title, False, (0, 0, 0))
+                surface.blit(rendered_palace, (10, line_position))
+                line_position += rendered_palace.get_height() + 10
+            return surface
 
         def get_from_palace(self):
-                dreddPalace = get_palace.Memory_Palace(0)
-                self.loci_list = dreddPalace.loci
-                self.facts_list = dreddPalace.facts
-                self.mnemonics_list = dreddPalace.mnemonics
-                self.image_files_list = dreddPalace.image_files
+                currentPalace = get_palace.Memory_Palace(self.select_which)
+                self.sheet_list = currentPalace.sheets
+                self.loci_list = currentPalace.loci
+                self.facts_list = currentPalace.facts
+                self.mnemonics_list = currentPalace.mnemonics
+                self.image_files_list = currentPalace.image_files
 
         def get_images(self):
-            for loci in self.loci_list:
-                self.loci_list_surf.append(self.font_1.render(loci, False, (0, 255, 0)))
-            for image in self.image_files_list:
-                filename = os.path.join('C:\\Users\\Luke\\Desktop\\Memory Palace\\Palaces\\Dredd', image)
-                self.image_list_surf.append(pygame.image.load(filename))
-            for fact in self.facts_list:
-                self.image_rect = self.image_list_surf[0].get_rect()
-                self.image_rect.width = self.image_rect.width - 200
-                self.image_rect.height = self.image_rect.height - 400
-                self.facts_list_surf.append(textrect.render_textrect(fact, self.font_2, self.image_rect, (255, 255, 255), (0, 0, 0), 1))
+            ###  @TODO We want get images to be called on start up only after the select menu ###
+            if self.start_up:
+                pass
+            elif self.start_up == False:
+                for loci in self.loci_list:
+                    self.loci_list_surf.append(self.font_1.render(loci, False, (0, 255, 0)))
+                for image in self.image_files_list:
+                    filename = os.path.join(self.folder_locations[0], image)
+                    self.image_list_surf.append(pygame.image.load(filename))
+                for fact in self.facts_list:
+                    self.image_rect = self.image_list_surf[0].get_rect()
+                    self.image_rect.width = self.image_rect.width - 200
+                    self.image_rect.height = self.image_rect.height - 400
+                    self.facts_list_surf.append(textrect.render_textrect(fact, self.font_2, self.image_rect, (255, 255, 255), (0, 0, 0), 1))
 
-            for mnemonic in self.mnemonics_list:
-                # self.mnemonics_list_surf.append(self.font_2.render(mnemonic, False, (139, 0, 139))) 
-                self.mnemonics_list_surf.append(textrect.render_textrect(mnemonic, self.font_2, self.image_rect, (255, 255, 255), (0, 0, 0), 0))
-      
+                for mnemonic in self.mnemonics_list:
+                    # self.mnemonics_list_surf.append(self.font_2.render(mnemonic, False, (139, 0, 139))) 
+                    self.mnemonics_list_surf.append(textrect.render_textrect(mnemonic, self.font_2, self.image_rect, (255, 255, 255), (0, 0, 0), 0))
+          
 class Viewer:
     def __init__(self):
         self.caption = "Memory Palace 3000"
